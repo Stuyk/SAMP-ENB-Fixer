0aba8f03bd29cb2bb17d2117e057253e10fb27d9
Trevor Wessel <stuykgaming@gmail.com>
2020-04-12 10:24:24 -0600

    Reg Fix

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const regedit = require('regedit');
 const randomToken = require('rand-token');
 const path = require('path');
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
 
 const username = process.env['USERPROFILE'].split(path.sep)[2];
 const registryPathsToRemove = [
@@ -58,38 +58,9 @@ async function runProcess() {
 
     statusMessage = `Updating registry entries...`;
     console.log(chalk.magentaBright(statusMessage));
+    const gtaSAPath = __dirname.replace('sampFixer', newFileEntry.fileName);
 
-    await new Promise((resolve) => {
-        regedit.list('HKCU\\Software\\SAMP', (err, res) => {
-            if (err) {
-                errorStatement = `Failed to find registry entry for SAMP under Computer\\HKEY_CURRENT_USER\\SOFTWARE\\SAMP`;
-                console.log(chalk.redBright(errorStatement));
-                process.exit(1);
-            }
-
-            const gtaSAPath = __dirname.replace('sampFixer', newFileEntry.fileName);
-            statusMessage = `Found registry entry for SAMP. Beginning update...`;
-            regedit.putValue(
-                {
-                    'HKCU\\Software\\SAMP': {
-                        gta_sa_exe: {
-                            value: gtaSAPath,
-                            type: 'REG_SZ',
-                        },
-                    },
-                },
-                (err) => {
-                    if (err) {
-                        console.log(err);
-                        process.exit(1);
-                    }
-                }
-            );
-
-            console.log('updated local path');
-            resolve();
-        });
-    });
+    execSync(`REG add "HKEY_CURRENT_USER\\Software\\SAMP" /v gta_sa_exe /d "${gtaSAPath}"`, { stdio: 'inherit' });
 
     statusMessage = `New GTA_SA was linked. Updating registry entires to fix ENB versioning.`;
     console.log(chalk.greenBright(statusMessage));
